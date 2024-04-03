97588b3b305d01d59af35554e2518fb354f22d5a
Alfredo Fernandez <aalagt@outlook.dk>
2024-04-03 20:02:18 +0200

    oprettet klasser til orderne.

---
+++ src/main/java/app/controllers/OrderController.java
@@ -0,0 +1,23 @@
+package app.controllers;
+
+public class OrderController {
+
+   private  Top top;
+   private  Botton botton;
+   private int quantity;
+
+
+    public Top getTop() {
+        return top;
+    }
+
+    public Botton getBotton() {
+        return botton;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+}
